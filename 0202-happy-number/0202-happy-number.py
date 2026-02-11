6e97f7624899af07d3159db75dfa86130b49c0b7
firomsa-m <sabboonaaar11@gmail.com>
2026-02-11 22:00:22 +0300

    Time: 7 ms (4.87%), Space: 19.4 MB (16.82%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        string=str(n)
+        for i in range(1,100):
+            count=0
+            for i in string:
+                count+=int(i)**2
+            if count==1 or count==7:
+                return True
+            elif count==n:
+                return False
+            string=str(count)
+        return False
+
+        

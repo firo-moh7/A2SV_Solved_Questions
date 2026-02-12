929b0463f0d5a0b5724e9a2a007abcdcf421ba45
firomsa-m <sabboonaaar11@gmail.com>
2026-02-12 11:07:59 +0300

    Time: 59 ms (28.07%), Space: 32.7 MB (93.87%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -2,6 +2,7 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if len(nums)==0:
             return 0
+        max_count=0
         nums.sort()
         count=0
         for i in range(len(nums)-1):
@@ -9,6 +10,9 @@ class Solution:
                 continue
             elif nums[i]+1==nums[i+1]:
                 count+=1
+            elif nums[i]+1!=nums[i+1]:
+                count=0
+            max_count=max(max_count,count)
 
-        return count+1
+        return max_count+1
         

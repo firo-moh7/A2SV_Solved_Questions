5f1b3f7fcf7f6ca26cfa288e586a010330279573
firomsa-m <sabboonaaar11@gmail.com>
2026-02-25 17:36:29 +0300

    Time: 54 ms (81.49%), Space: 29.4 MB (16.85%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -8,7 +8,7 @@ class Solution:
             cur_sum+=nums[i]
             cur_sum-=nums[i-k]
             max_sum=max(cur_sum,max_sum)
-        return max_sum/4
+        return max_sum/k
             
 
 

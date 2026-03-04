2a54b464c80417edf9a6b5c58b8456f227a3148b
firomsa-m <sabboonaaar11@gmail.com>
2026-03-04 17:35:51 +0300

    Time: 59 ms (59.17%), Space: 42.9 MB (58.92%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        arr=[0]*(len(s)+1)
+        for l,r,k in shifts:
+            if k==0:
+                arr[l]-=1
+                arr[r+1]+=1
+            else:
+                arr[l]+=1
+                arr[r+1]-=1
+        
+        cur=0
+        res=[0]*len(s)
+        for i in range(len(s)):
+            cur+=arr[i]
+            res[i]=cur
+        fin_ans=""
+        for i in range (len(s)):
+            ans=ord(s[i])-ord('a')
+            ans=(ans+res[i])%26
+            fin_ans+=chr(ans+ord('a'))
+        return fin_ans
+
+
+            
+
+            
+
+            
+        

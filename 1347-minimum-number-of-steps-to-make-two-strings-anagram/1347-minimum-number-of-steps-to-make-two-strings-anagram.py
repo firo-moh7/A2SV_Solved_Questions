6df205b2eafce42d5ced495ac88114424e8d4445
firomsa-m <sabboonaaar11@gmail.com>
2026-03-06 23:28:21 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,12 +1,12 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        dict_s=Counter(s)
-        dict_t=Counter(t)
+        freq_s=Counter(s)
+        freq_t=Counter(t)
 
         count=0
-        for key in dict_s:
-            if dict_s[key]>dict_t[key]:
-                count+=(dict_s[key]-dict_t[key])
+        for key in freq_s:
+            if freq_s[key]>freq_t[key]:
+                count+=(freq_s[key]-freq_t[key])
         return count
 
 

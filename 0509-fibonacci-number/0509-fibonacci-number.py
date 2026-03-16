8219bdac1564d57f016a029a4a15bfde08860f4f
firomsa-m <sabboonaaar11@gmail.com>
2026-03-16 16:58:42 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n==0 or n==1: 
+        if n<2: 
             return n
-        
-            
+    
         return self.fib(n-1)+self.fib(n-2)
         

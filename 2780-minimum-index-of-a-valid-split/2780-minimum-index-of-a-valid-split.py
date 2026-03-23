01d30e6ad19346137c52c9bbf2449a11204a9c42
firomsa-m <sabboonaaar11@gmail.com>
2026-03-23 10:09:54 +0300

    Time: 115 ms (21.81%), Space: 35.8 MB (17.16%) - LeetHub

---
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+        first_freq=Counter(nums)
+        second_freq=defaultdict(int)
+        n=len(nums)
+        
+        for index in range(len(nums)):
+            num=nums[index]
+            first_freq[num]-=1
+            second_freq[num]+=1
+
+            if second_freq[num] * 2 > index + 1 and first_freq[num] * 2 > n - index -1:
+                return index
+        
+        return -1
+
+        

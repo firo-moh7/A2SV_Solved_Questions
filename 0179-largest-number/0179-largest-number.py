b1f714def0e8207cfd822c96c86b764333620892
firomsa-m <sabboonaaar11@gmail.com>
2026-03-27 17:44:53 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        #l=set(nums)
-        #if len(l)==1 and 0 in l:
-           # return "0"
+        l=set(nums)
+        if len(l)==1 and 0 in l:
+            return "0"
 
         temp=[]
         for i in nums:
@@ -10,8 +10,6 @@ class Solution:
         from functools import cmp_to_key
         temp.sort(key=cmp_to_key(lambda x,y: -1 if x+y > y+x else 1))
 
-        if temp[0]=="0":
-            return "0"
         return "".join(temp)
 
 

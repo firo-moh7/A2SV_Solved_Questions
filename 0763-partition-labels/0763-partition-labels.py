706b926ddcc3dd5f98fae620e385421c2590b9cd
firomsa-m <sabboonaaar11@gmail.com>
2026-04-21 17:59:47 +0300

    Time: 0 ms (100%), Space: 19.3 MB (66.83%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_oc = {}
+
+        for i , char in enumerate(s):
+            last_oc[char] = i
+        print(last_oc)
+        result = []
+        start = 0
+        end = 0
+
+        for i , char in enumerate(s):
+            end = max(end , last_oc[char])
+
+            if i == end:
+                result.append(end - start + 1)
+                start = i + 1
+
+        return result
+        
+        
